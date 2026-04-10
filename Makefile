34eafed7e27fce9f31b9702ee7c9c16a9ae4135c
raykpridgen <rkpridgen@coastal.edu>
2026-04-10 00:10:31 -0400

    Added verify

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -Wall -Wstrict-prototypes -std=gnu99
 
 UTILS = utilities.c
 
-all: make-2d print-2d stencil-2d stencil-2d-pth
+all: make-2d print-2d stencil-2d stencil-2d-pth verify
 
 make-2d: make-2d.c $(UTILS)
 	$(CC) $(CFLAGS) -o $@ $^
@@ -17,5 +17,8 @@ stencil-2d: stencil-2d.c $(UTILS)
 stencil-2d-pth: stencil-2d-pth.c $(UTILS)
 	$(CC) $(CFLAGS) -pthread -o $@ $^
 
+verify: verify.c $(UTILS)
+	$(CC) $(CFLAGS) -o $@ $^
+
 clean:
-	rm -f make-2d print-2d stencil-2d stencil-2d-pth *.o *.dat
+	rm -f make-2d print-2d stencil-2d stencil-2d-pth verify *.o *.dat
---
+++ verify.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare_files(const char *f1, const char *f2)
+{
+    FILE *fp1 = fopen(f1, "rb");
+    FILE *fp2 = fopen(f2, "rb");
+
+    if (!fp1 || !fp2)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    int rows1, cols1, rows2, cols2;
+
+    fread(&rows1, sizeof(int), 1, fp1);
+    fread(&cols1, sizeof(int), 1, fp1);
+
+    fread(&rows2, sizeof(int), 1, fp2);
+    fread(&cols2, sizeof(int), 1, fp2);
+
+    if (rows1 != rows2 || cols1 != cols2)
+    {
+        printf("Different dimensions: (%d x %d) vs (%d x %d)\n",
+               rows1, cols1, rows2, cols2);
+        fclose(fp1);
+        fclose(fp2);
+        return 0;
+    }
+
+    int total = rows1 * cols1;
+    int diff_count = 0;
+
+    for (int i = 0; i < total; i++)
+    {
+        double v1, v2;
+
+        fread(&v1, sizeof(double), 1, fp1);
+        fread(&v2, sizeof(double), 1, fp2);
+
+        if (v1 != v2)
+        {
+            diff_count++;
+        }
+    }
+
+    if (diff_count == 0)
+    {
+        printf("Files are identical (%d cells checked)\n", total);
+    }
+    else
+    {
+        printf("Files differ: %d / %d cells mismatch\n", diff_count, total);
+    }
+
+    fclose(fp1);
+    fclose(fp2);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s file1.dat file2.dat\n", argv[0]);
+        return 1;
+    }
+
+    return compare_files(argv[1], argv[2]);
+}
