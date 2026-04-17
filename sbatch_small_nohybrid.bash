25fdb7a2548c7392c98f41415ad7a8aa28fe19cd
ronibutbul <rbutbul@coastal.edu>
2026-04-17 14:24:16 -0400

    mini test

--- sbatch_small_nohybrid.bash
+++ sbatch_small_nohybrid.bash
@@ -15,7 +15,12 @@ module load slurm
 module load gcc/10.2.0
 module load openmpi/4.1.1
 
-echo "Compiling code..."
+echo "Current directory:"
+pwd
+echo "Files:"
+ls -l
+
+echo "Compiling..."
 make clean
 make
 
@@ -24,33 +29,33 @@ COLS=512
 ITERS=10
 
 echo "Creating input matrix..."
-./make-2d $ROWS $COLS input-512.dat
+./build/make-2d $ROWS $COLS input-512.dat
 
 echo "Running serial..."
-./stencil-2d -n $ITERS -i input-512.dat -o out-serial.dat
+./build/stencil-2d -n $ITERS -i input-512.dat -o out-serial.dat
 
 echo "Running pthreads..."
-./stencil-2d-pth -t $ITERS -i input-512.dat -o out-pth.dat -p 4
+./build/stencil-2d-pth -t $ITERS -i input-512.dat -o out-pth.dat -p 4
 
 echo "Running OpenMP..."
 export OMP_NUM_THREADS=4
-./stencil-2d-omp -n $ITERS -i input-512.dat -o out-omp.dat
+./build/stencil-2d-omp -n $ITERS -i input-512.dat -o out-omp.dat
 
 echo "Running MPI..."
-mpirun -np 4 ./stencil-2d-mpi -t $ITERS -i input-512.dat -o out-mpi.dat
+mpirun -np 4 ./build/stencil-2d-mpi -t $ITERS -i input-512.dat -o out-mpi.dat
 
 echo "Running hybrid..."
-mpirun -np 2 ./stencil-2d-hybrid -t $ITERS -i input-512.dat -o out-hybrid.dat -p 2
+mpirun -np 2 ./build/stencil-2d-hybrid -t $ITERS -i input-512.dat -o out-hybrid.dat -p 2
 
 echo "Verifying outputs..."
 
 echo "serial vs pthreads"
-./verify out-serial.dat out-pth.dat
+./build/verify out-serial.dat out-pth.dat
 
 echo "serial vs openmp"
-./verify out-serial.dat out-omp.dat
+./build/verify out-serial.dat out-omp.dat
 
 echo "serial vs mpi"
-./verify out-serial.dat out-mpi.dat
+./build/verify out-serial.dat out-mpi.dat
 
 echo "Done."
